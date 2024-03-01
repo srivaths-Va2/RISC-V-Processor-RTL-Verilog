627daee0ddc509e53f36cb1dab6be9e2e4fe1772
Srivaths Ramasubramanian <68600797+srivaths-Va2@users.noreply.github.com>
2024-03-01 17:11:34 +0530

    added control logic for I type instructions

--- Control/CONTROL_R.v
+++ Control/CONTROL_R.v
@@ -11,7 +11,15 @@ The module outputs control instructions for-
 5) S-type instructions
 6) B-type instructions
 
+the signal inst_type gives information of the encoding of the instruction. 
 
+inst_type       |       Encoding
+3'b000                  R
+3'b001                  U
+3'b010                  J
+3'b011                  I
+3'b100                  S
+3'b101                  B
 
 */
 
@@ -19,14 +27,17 @@ The module outputs control instructions for-
 module CONTROL_R(
     input[31:0] instruction_word,
     output reg[3:0] alu_ctrl,
-    output reg reg_write
+    output reg shamt_en,        // control strobe for shift amount for I-type instructions
+    output reg reg_write,
+    output reg[2:0] inst_type
 );
     
-    always@(instruction_word[6:0] or instruction_word[14:12] or instruction_word[31:25])
+    always@(instruction_word)
         begin
             //coding for R type instructions
             if(instruction_word[6:0] == 7'b0110011) begin
                 reg_write = 1;
+                inst_type = 3'b000;
 
                 case(instruction_word[14:12])
                     3'b000 : begin
@@ -51,15 +62,46 @@ module CONTROL_R(
                 endcase
             end
             
-           /*
-            // Coding for U-type instructions
-            if(opcode == 7'b0110111)
-                case
+            
+            /*
+            else if(instruction_word[6:0] == 7'b0000011)
+                begin
+                    reg_write = 1'b1;
+                    //r_or_w = 1'b0;      // write operation to store value enabled (LUI and AUIPC)
+                    inst_type = 3'b011;
+                end
+                */
+            
+            // coding for I-type instructions
+            else if(instruction_word[6:0] == 7'b0000011)
+                begin
+                    reg_write = 1'b1;
+                    inst_type = 3'b011;
+                    shamt_en = 1'b0;
+                    alu_ctrl = 4'b0000;
 
-                endcase
+                    case(instruction_word[14:12])
+                        3'b000 : alu_ctrl = 4'b0010;
+                        3'b001 : begin
+                            alu_ctrl = 4'b0011;
+                            shamt_en = 1'b1;
+                        end
+                        3'b010 : alu_ctrl = 4'b1000;
+                        3'b011 : alu_ctrl = 4'b0111;
+                        3'b100 : alu_ctrl = 4'b0001;
+                        3'b101 : begin
+                            shamt_en = 1'b1;
+                            if(instruction_word[31:25] == 7'b0000000)
+                                assign alu_ctrl = 4'b0101;
+                            else if(instruction_word[31:25] == 7'b0100000)
+                                assign alu_ctrl = 4'b1001;
+                        end
+                        3'b110 : alu_ctrl = 4'b0001;
+                        3'b111 : alu_ctrl = 4'b0010;
+                    endcase
 
-        end
-        */
+                end
+                
         end
 
 endmodule
--- Control/CONTROL_R_tb.v
+++ Control/CONTROL_R_tb.v
@@ -4,12 +4,16 @@
 module CONTROL_R_tb;
 reg[31:0] instruction_word;
 wire[3:0] alu_ctrl;
+wire shamt_en;
 wire reg_write;
+wire[2:0] inst_type;
 
 CONTROL_R control_unit (
     .instruction_word(instruction_word),
     .alu_ctrl(alu_ctrl),
-    .reg_write(reg_write)
+    .shamt_en(shamt_en),
+    .reg_write(reg_write),
+    .inst_type(inst_type)
 );
 
 /*
@@ -24,6 +28,7 @@ begin
     $dumpfile("CONTROL_R_tb.vcd");
     $dumpvars(0, CONTROL_R_tb);
 
+    // R type instructions testing
     instruction_word = 32'b0000000_00100_10101_000_00101_0110011;
     #20;
 
@@ -31,6 +36,19 @@ begin
     #20;
 
     instruction_word = 32'b0000000_00100_10101_001_00101_0110011;
+    #20;
+
+    // I type instructions testing
+    instruction_word = 32'b001000001001_10011_000_00111_0000011;
+    #20;
+
+    instruction_word = 32'b011101101101_00001_111_00110_0000011;
+    #20;
+
+    instruction_word = 32'b000011110101_01101_101_01101_0000011;
+    #20;
+
+    instruction_word = 32'b000011110101_01101_011_01101_0000011;
     #20;
 
     $display("Test Complete");
