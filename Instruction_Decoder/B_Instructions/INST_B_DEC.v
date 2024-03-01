1f593a2b3208574e618d1735644739099e329317
Srivaths Ramasubramanian <68600797+srivaths-Va2@users.noreply.github.com>
2024-03-01 23:36:05 +0530

    added branch decoder

---
+++ Instruction_Decoder/B_Instructions/INST_B_DEC.v
@@ -0,0 +1,22 @@
+/*
+This module can perform instruction decoding for all B-type instructions
+The module outputs the register addresses, MSB and LSB immediate values after decoding
+*/
+
+module INST_B_DEC (
+    input[31:0] instruction_word,
+    output reg[6:0] imm_MSB,
+    output reg[4:0] rs2,
+    output reg[4:0] rs1,
+    output reg[4:0] imm_LSB
+);
+
+    always@(instruction_word)
+        begin
+            imm_MSB = instruction_word[31:25];
+            rs2 = instruction_word[24:20];
+            rs1 = instruction_word[19:15];
+            imm_LSB = instruction_word[11:7];
+        end
+    
+endmodule
---
+++ Instruction_Decoder/B_Instructions/INST_B_DEC_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns/1ns
+`include "INST_B_DEC.v"
+
+module INST_B_DEC_tb;
+reg[31:0] instruction_word;
+wire[6:0] imm_MSB;
+wire[4:0] rs2;
+wire[4:0] rs1;
+wire[4:0] imm_LSB;
+
+INST_B_DEC B_Decoder(instruction_word, imm_MSB, rs2, rs1, imm_LSB);
+
+initial
+    begin
+        $dumpfile("INST_B_DEC_tb.vcd");
+        $dumpvars(0, INST_B_DEC_tb);
+
+        // B-type instructions
+        instruction_word = 32'b0000111_10101_01101_111_01101_1100011;
+        #20;
+
+        instruction_word = 32'b0000111_10101_01101_100_01101_1100011;
+        #20;
+
+
+    end
+
+endmodule
